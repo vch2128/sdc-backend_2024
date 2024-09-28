4885d161cd7a4a25ea55cc41ddaa7ed5728063aa
vch2128 <bonniechuang920915@gmail.com>
2024-09-28 22:44:37 +0800

    add vch2128 homework2

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
+from typing import Annotated
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+            "message": "Hello World"
+            }
+
+
+@app.put("/items/{item_id}")
+async def read_items(
+                        item_id: Annotated[int, Path()],
+                        q: Annotated[str | None, Query()] = None
+                    ):
+    results = {
+                "item_id": item_id,
+                "name": "Test Item",
+                "description": "A test description",
+                "price": 10.5,
+                "tax": 1.5
+                }
+    if q:
+        results.update({"q": q})
+
+    return results
